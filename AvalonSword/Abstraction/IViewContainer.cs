8717c6c65384ece7913b515407854b4ba1770453
durow <durow@163.com>
2017-02-15 17:50:09 +0800

    update IViewContainer and ViewContainer

--- AvalonSword/Abstraction/IViewContainer.cs
+++ AvalonSword/Abstraction/IViewContainer.cs
@@ -12,13 +12,86 @@ namespace Ayx.AvalonSword
     {
         int Count { get; }
 
-        void BindView<TView, TViewModel>()
-            where TView : FrameworkElement where TViewModel : class;
+        #region CreateView
 
         FrameworkElement CreateViewFromModel<TViewModel>()
             where TViewModel : class;
 
-        TView CreateView<TView>() where TView : FrameworkElement;
+        TView CreateView<TView>() 
+            where TView : FrameworkElement;
+
+        TView CreateView<TView, TViewModel>() 
+            where TView : FrameworkElement 
+            where TViewModel : class;
+
+        TView CreateView<TView, TViewModel>(TViewModel viewmodel) 
+            where TView : FrameworkElement 
+            where TViewModel : class;
+
+        #endregion
+
+        #region CreateWindow
+
+        TView CreateWindow<TView>()
+            where TView : Window;
+
+        Window CreateWindowFromModel<TViewModel>()
+            where TViewModel : class;
+
+        TView CreateWindow<TView, TViewModel>()
+            where TView : Window
+            where TViewModel : class;
+
+        TView CreateWindow<TView, TViewModel>(TViewModel viewmodel)
+            where TView : Window
+            where TViewModel : class;
+
+        #endregion
+
+        #region ShowWindow
+
+        TView ShowWindow<TView>(Window owner = null) 
+            where TView : Window;
+
+        Window ShowWindowFromModel<TViewModel>(Window owner = null)
+            where TViewModel : class;
+
+        TView ShowWindow<TView, TViewModel>(Window owner = null) 
+            where TView : Window 
+            where TViewModel : class;
+
+        TView ShowWindow<TView, TViewModel>(TViewModel viewmodel, Window owner = null) 
+            where TView : Window 
+            where TViewModel : class;
+
+        #endregion ShowWindow
+
+        #region ShowDialog
+
+        TView ShowDialog<TView>(Window owner = null)
+            where TView : Window;
+
+        Window ShowDialogFromModel<TViewModel>(Window owner = null)
+            where TViewModel : class;
+
+        TView ShowDialog<TView, TViewModel>(Window owner = null)
+            where TView : Window
+            where TViewModel : class;
+
+        TView ShowDialog<TView, TViewModel>(TViewModel viewmodel, Window owner = null)
+            where TView : Window
+            where TViewModel : class;
+
+        #endregion
+
+        #region OthreMethods
+
+        void BindView<TView, TViewModel>()
+            where TView : FrameworkElement
+            where TViewModel : class;
+
+        TViewModel GetViewModel<TViewModel>()
+            where TViewModel : class;
 
         void RemoveViewModel<TViewModel>()
             where TViewModel : class;
@@ -26,8 +99,12 @@ namespace Ayx.AvalonSword
         void RemoveView<TView>()
             where TView : FrameworkElement;
 
-        bool ContainsView<TView>() where TView : FrameworkElement;
+        bool ContainsView<TView>()
+            where TView : FrameworkElement;
 
-        bool ContainsViewModel<TViewModel>() where TViewModel : class;
+        bool ContainsViewModel<TViewModel>()
+            where TViewModel : class;
+
+        #endregion
     }
 }
--- AvalonSword/ViewContainer.cs
+++ AvalonSword/ViewContainer.cs
@@ -29,28 +29,22 @@ namespace Ayx.AvalonSword
             this.serviceContainer = serviceContainer;
         }
 
-        public void BindView<TView, TViewModel>()
-            where TView : FrameworkElement
-            where TViewModel : class
-        {
-            var vmType = typeof(TViewModel);
-            if (VMList.ContainsKey(vmType))
-                throw new Exception($"{nameof(vmType)} have binded!");
-            VMList.Add(vmType, typeof(TView));
-        }
+        #region CreateView
 
-        public TView CreateView<TView>() where TView : FrameworkElement
+        public TView CreateView<TView>()
+            where TView : FrameworkElement
         {
             var view = serviceContainer.GetService<TView>();
             var vmType = GetViewModelFromView<TView>();
             if (vmType == null) return view;
-            
+
             var vm = serviceContainer.GetService(vmType);
             view.DataContext = vm;
             return view;
         }
 
-        public FrameworkElement CreateViewFromModel<TViewModel>() where TViewModel : class
+        public FrameworkElement CreateViewFromModel<TViewModel>()
+            where TViewModel : class
         {
             var vmType = typeof(TViewModel);
             if (!VMList.ContainsKey(vmType))
@@ -62,31 +56,187 @@ namespace Ayx.AvalonSword
             return view;
         }
 
-        public void RemoveView<TView>() where TView : FrameworkElement
+        public TView CreateView<TView, TViewModel>()
+            where TView : FrameworkElement
+            where TViewModel : class
+        {
+            var vm = serviceContainer.GetService<TViewModel>();
+            return CreateView<TView, TViewModel>(vm);
+        }
+
+        public TView CreateView<TView, TViewModel>(TViewModel viewmodel)
+            where TView : FrameworkElement
+            where TViewModel : class
+        {
+            var view = serviceContainer.GetService<TView>();
+            view.DataContext = viewmodel;
+            return view;
+        }
+
+        #endregion
+
+        #region CreateWindow
+
+        public TView CreateWindow<TView>()
+            where TView : Window
+        {
+            var win = CreateView<TView>();
+            return win;
+        }
+
+        public Window CreateWindowFromModel<TViewModel>() where TViewModel : class
+        {
+            var view = CreateViewFromModel<TViewModel>();
+            return ConvertToWindow(view);
+        }
+
+        public TView CreateWindow<TView, TViewModel>()
+            where TView : Window
+            where TViewModel : class
+        {
+            return CreateView<TView, TViewModel>();
+        }
+
+        public TView CreateWindow<TView, TViewModel>(TViewModel viewmodel)
+            where TView : Window
+            where TViewModel : class
+        {
+            return CreateView<TView, TViewModel>(viewmodel);
+        }
+
+        #endregion
+
+        #region ShowWindow
+
+        public TView ShowWindow<TView>(Window owner = null) 
+            where TView : Window
+        {
+            var win = CreateWindow<TView>();
+            win.Owner = owner;
+            win.Show();
+            return win;
+        }
+
+        public Window ShowWindowFromModel<TViewModel>(Window owner = null) 
+            where TViewModel : class
+        {
+            var win = CreateWindowFromModel<TViewModel>();
+            win.Owner = owner;
+            win.Show();
+            return win;
+        }
+
+        public TView ShowWindow<TView, TViewModel>(Window owner = null)
+           where TView : Window
+           where TViewModel : class
+        {
+            var win = CreateWindow<TView, TViewModel>();
+            win.Owner = owner;
+            win.Show();
+            return win;
+        }
+
+        public TView ShowWindow<TView, TViewModel>(TViewModel viewmodel, Window owner = null)
+            where TView : Window
+            where TViewModel : class
+        {
+            var win = CreateWindow<TView, TViewModel>(viewmodel);
+            win.Owner = owner;
+            win.Show();
+            return win;
+        }
+        #endregion
+
+        #region ShowDialog
+        public TView ShowDialog<TView>(Window owner = null) 
+            where TView : Window
+        {
+            var win = CreateWindow<TView>();
+            win.Owner = owner;
+            win.ShowDialog();
+            return win;
+        }
+
+        public Window ShowDialogFromModel<TViewModel>(Window owner = null) 
+            where TViewModel : class
+        {
+            var win = CreateWindowFromModel<TViewModel>();
+            win.Owner = owner;
+            win.ShowDialog();
+            return win;
+        }
+
+        public TView ShowDialog<TView, TViewModel>(Window owner = null)
+            where TView : Window
+            where TViewModel : class
+        {
+            var win = CreateWindow<TView, TViewModel>();
+            win.Owner = owner;
+            win.ShowDialog();
+            return win;
+        }
+
+        public TView ShowDialog<TView, TViewModel>(TViewModel viewmodel, Window owner = null)
+            where TView : Window
+            where TViewModel : class
+        {
+            var win = CreateWindow<TView, TViewModel>(viewmodel);
+            win.Owner = owner;
+            win.ShowDialog();
+            return win;
+        }
+
+        #endregion
+
+        #region OtherMethos
+        public void BindView<TView, TViewModel>()
+            where TView : FrameworkElement
+            where TViewModel : class
+        {
+            var vmType = typeof(TViewModel);
+            if (VMList.ContainsKey(vmType))
+                throw new Exception($"{nameof(vmType)} have binded!");
+            VMList.Add(vmType, typeof(TView));
+        }
+
+        public void RemoveView<TView>()
+            where TView : FrameworkElement
         {
             var vmType = GetViewModelFromView<TView>();
-            if(vmType != null)
+            if (vmType != null)
             {
                 VMList.Remove(vmType);
             }
         }
 
-        public void RemoveViewModel<TViewModel>() where TViewModel : class
+        public void RemoveViewModel<TViewModel>()
+            where TViewModel : class
         {
             var vmType = typeof(TViewModel);
             if (VMList.ContainsKey(vmType))
                 VMList.Remove(vmType);
         }
 
-        public bool ContainsView<TView>() where TView : FrameworkElement
+        public bool ContainsView<TView>()
+            where TView : FrameworkElement
         {
             return GetViewModelFromView<TView>() != null;
         }
 
-        public bool ContainsViewModel<TViewModel>() where TViewModel : class
+        public bool ContainsViewModel<TViewModel>()
+            where TViewModel : class
         {
             return VMList.ContainsKey(typeof(TViewModel));
         }
+
+        public TViewModel GetViewModel<TViewModel>() where TViewModel : class
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region PrivateMethods
 
         private Type GetViewModelFromView<TView>()
         {
@@ -98,5 +248,17 @@ namespace Ayx.AvalonSword
 
             return null;
         }
+
+        private Window ConvertToWindow(FrameworkElement view)
+        {
+            var win = view as Window;
+            if (win == null)
+                throw new Exception($"{view.GetType()} is not a Window!");
+
+            return win;
+        }
+
+        #endregion
+
     }
 }
