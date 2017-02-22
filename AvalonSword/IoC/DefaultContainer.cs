fe63265469ada96f82852402c51a47a915c2748b
durow <durow@163.com>
2017-02-22 09:58:03 +0800

    add AyxContainer

---
+++ AvalonSword/IoC/DefaultContainer.cs
@@ -0,0 +1,35 @@
+﻿using Ayx.AvalonSword.Abstraction;
+using System;
+
+namespace Ayx.AvalonSword.IoC
+{
+    public class DefaultContainer : IServiceContainer
+    {
+        private AyxContainer container = new AyxContainer();
+
+        public void Add<TInterface, TService>() where TService : TInterface
+        {
+            container.Wire<TInterface, TService>();
+        }
+
+        public void AddSingleton<TInterface>(TInterface item)
+        {
+            container.WireSingleton<TInterface>(item);
+        }
+
+        public void AddSingleton<TInterface, TService>() where TService : TInterface
+        {
+            container.WireSingleton<TInterface, TService>();
+        }
+
+        public object GetService(Type type)
+        {
+            return container.Get(type);
+        }
+
+        public TInterface GetService<TInterface>()
+        {
+            return container.Get<TInterface>();
+        }
+    }
+}
