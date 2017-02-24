7c05b4282d58fc19e7ce7a541befdcce0a66a26f
durow <durow@163.com>
2017-02-24 20:15:25 +0800

    add NoReturnConverter

---
+++ AvalonSword/Converters/NoReturnConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Ayx.AvalonSword.Converters
+{
+    public class NoReturnConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString().Replace("\r", "").Replace("\n", "");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
